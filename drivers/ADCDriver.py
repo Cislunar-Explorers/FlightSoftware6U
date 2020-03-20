3a570e81d3d5f3a70bcd55c989ae10e66c4e4ebc
Stefan Brechter <scb262@cornell.edu>
2020-03-20 15:57:57 -0400

    added init and readPressure

--- drivers/ADCDriver.py
+++ drivers/ADCDriver.py
@@ -8,3 +8,17 @@
 # Cornell University
 
 import ADS1115
+import time
+
+
+class ADC:
+    def init(self, ads):
+        ads = ADS1115.ADS1115()
+
+    # Read the pressure from the pressure transducer at channel 0 on the ads1115
+    def readPressure(self, ads):
+        # ads.config = ads.__ADS1015_REG_CONFIG_MUX_SINGLE_0
+        milVolts = ads.readADCSingleEnded(channel=0)
+        pressure = milVolts / 5 * 300
+        return pressure
+

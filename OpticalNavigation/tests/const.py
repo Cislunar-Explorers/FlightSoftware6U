7e5b28d4dbf93fe9e0753dcb222b30bff98d401c
Eashaan Kumar <eashaan@kumar.co>
2020-03-24 20:45:13 -0400

    ukf dynamics model does not converge

---
+++ OpticalNavigation/tests/const.py
@@ -0,0 +1,9 @@
+TEST_ECLIPSEANDCRESCENTIMAGES = 'C:\\Users\\easha\\Downloads\\EclipseAndCrescentImages'
+TEST_FIND_DATASET_IMAGE_DIR = '\\images'
+TEST_FIND_DATASET_CIRCLES_DIR = '\\circles\\circles.csv'
+
+TEST_EM1_3DOF_Trajectory_June_27_2020_3600sec = 'C:\\Users\\easha\\Downloads\\EM1_3DOF_Trajectory_June_27_2020_3600sec'
+TEST_EM1_3DOF_Trajectory_June_27_2020_3600sec_iterations = '\\iterations'
+TEST_EM1_3DOF_Trajectory_June_27_2020_3600sec_trajectory = '\\trajectory\\trajectory.csv'
+TEST_EM1_3DOF_Trajectory_June_27_2020_3600sec_moonEph = '\\ephemeris\\moon_eph.csv'
+TEST_EM1_3DOF_Trajectory_June_27_2020_3600sec_sunEph = '\\ephemeris\\sun_eph.csv'

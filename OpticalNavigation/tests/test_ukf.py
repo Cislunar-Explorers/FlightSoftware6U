narrowed down bug to covariance matrix multiplication in newEstimate

OpticalNavigation/tests/test_ukf.py
@@ -5,6 +5,7 @@ import math
 import os
 from core.ukf import runUKF
 from tests.const import POS_ERROR, VEL_ERROR
+from tests.const import MatlabTestCameraParameters
 
 
 def test_dynamics_model_EM1_3DOF_Trajectory_June_27_2020_3600sec():
@@ -96,7 +97,7 @@ def test_ukf_6hours():
         moonEph = (np.array([moonEphdf.iloc[t]['x'], moonEphdf.iloc[t]['y'], moonEphdf.iloc[t]['z'], moonEphdf.iloc[t]['vx'], moonEphdf.iloc[t]['vy'], moonEphdf.iloc[t]['vz']], dtype=np.float)).reshape(1,6)
         sunEph = (np.array([sunEphdf.iloc[t]['x'], sunEphdf.iloc[t]['y'], sunEphdf.iloc[t]['z'], sunEphdf.iloc[t]['vx'], sunEphdf.iloc[t]['vy'], sunEphdf.iloc[t]['vz']], dtype=np.float)).reshape(1,6)
         meas = (np.array([measEphdf.iloc[t]['z1'], measEphdf.iloc[t]['z2'], measEphdf.iloc[t]['z3'], measEphdf.iloc[t]['z4'], measEphdf.iloc[t]['z5'], measEphdf.iloc[t]['z6']], dtype=np.float)).reshape(6,1)
-        state, P, K = runUKF(moonEph, sunEph, meas, state, 60, P, dynamicsOnly=False)
+        state, P, K = runUKF(moonEph, sunEph, meas, state, 60, P, MatlabTestCameraParameters, dynamicsOnly=False)
     t = trajTruthdf.shape[0] - 1
     traj = (np.array([trajTruthdf.iloc[t]['x'], trajTruthdf.iloc[t]['y'], trajTruthdf.iloc[t]['z'], trajTruthdf.iloc[t]['vx'], trajTruthdf.iloc[t]['vy'], trajTruthdf.iloc[t]['vz']], dtype=np.float)).reshape(6,1)
         
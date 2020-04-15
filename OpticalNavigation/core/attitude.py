5090ffa338b63c07a415dbbdd90d4df9384f9eb5
Eashaan Kumar <eashaan@kumar.co>
2020-04-15 14:49:25 -0400

    Hunter's quaternion estimation code

---
+++ OpticalNavigation/core/attitude.py
@@ -0,0 +1,10 @@
+import numpy
+from mpl_toolkits.mplot3d import Axes3D
+from numpy.linalg import inv
+from numpy.linalg import pinv
+from scipy.stats import norm
+from scipy import integrate
+from numpy import random
+from copy import deepcopy
+from scipy.interpolate import InterpolatedUnivariateSpline
+

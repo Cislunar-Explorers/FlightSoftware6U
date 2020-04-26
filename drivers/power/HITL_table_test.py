b33399118bd7b606f2825fed6745655add55770b
Toby <fischer.m.tobias@gmail.com>
2020-04-26 14:54:49 -0400

    HITL test script continued

--- drivers/power/HITL_table_test.py
+++ drivers/power/HITL_table_test.py
@@ -6,15 +6,22 @@ import time
 
 HITL_test = Power()
 
-print(" --- TESTING  displayAll --- ")
+print("Turning off all outputs")
+OUTPUTS = [OUT_1, OUT_2, OUT_3, OUT_4, OUT_5, OUT_6]
+for i in range(1,6):
+    HITL_test.set_single_output(OUTPUTS[i], 0,0)
+
+print(" --- TESTING  displayAll --- \n")
 HITL_test.displayAll()
 
-# Turn every channel on then off sequentially using set_single_output
-OUTPUTS = [OUT_1, OUT_2, OUT_3, OUT_4, OUT_5, OUT_6]
+print("\nBeginning output testing in 5 seconds\n")
+time.sleep(5)
 
+# Turn every channel on then off sequentially using set_single_output
+print("\n --- TESTING OUPUTS --- \n")
 for i in range(1, 6):
     current_output = OUTPUTS[i]
-    print(" ### TESTING OUT_" + str(i) + " ####")
+    print(" ### TESTING OUT_" + str(i) + " ###\n")
     HITL_test.set_single_output(current_output, 1, 0)  # Turns on channel
     time.sleep(1)  # wait one second
     HK_data = HITL_test.get_hk_1()  # get the housekeeping data
@@ -26,19 +33,23 @@ for i in range(1, 6):
 # Test the component-functions
 # test burnwire:
 # TODO: Check with Aaron (either one) about software requirements (i.e. what data the component functions should return)
-print("Testing COMPONENT FUNCTIONS")
-
+print("Testing component functions in 5 seconds")
+print("\n--- TESTING COMPONENT FUNCTIONS --- \n")
+time.sleep(5)
 print("Testing burnwire:")
 print("You should see HITL outputs 9 and 10 light up")
 HITL_test.burnwire(1)
-
-print("Testing Solenoid")
-print("You should see HITL output 12 light up")
-HITL_test.solenoid(10,990)
+time.sleep(1)
 
 print("Testing Glowplug")
 print("You should see output 11 light up")
 HITL_test.glowplug(1)
+time.sleep(1)
+
+print("Testing Solenoid")
+print("You should see HITL output 12 light up")
+HITL_test.solenoid(10,990)
+time.sleep(1)
 
 print("Testing Electrolyzer")
 print("You should see HITL output 13 light up for 10 seconds")
@@ -46,4 +57,4 @@ HITL_test.electrolyzer(True)
 time.sleep(10)
 HITL_test.electrolyzer(False)
 
-print("Component function testing done")
+print("\nComponent function testing done")

f9ff2cc1849340bab8ee57de8039f3bcba4df312
Toby <fischer.m.tobias@gmail.com>
2020-05-05 10:56:52 -0400

    Added higher-level driver

--- drivers/power/power_controller.py
+++ drivers/power/power_controller.py
@@ -16,10 +16,7 @@ from power_structs import *
 import power_structs as ps
 import RPi.GPIO as GPIO
 import time
-<<<<<<< HEAD
 from enum import Enum
-=======
->>>>>>> Preparation for HITL testing
 
 # power device address
 POWER_ADDRESS           = 0x02

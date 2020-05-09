214f373b8191599559258c221a2691c33f295b77
Stefan Brechter <scb262@cornell.edu>
2020-05-09 12:11:18 -0400

    temperature

--- drivers/ADCDriver.py
+++ drivers/ADCDriver.py
@@ -18,21 +18,20 @@ class ADC:
 
     # Read the pressure from the pressure transducer at channel 0 on the ads1115
     def readPressure(self):
-        # ads.config = ads.__ADS1015_REG_CONFIG_MUX_SINGLE_0
+        self.config = ADS1115.ADS1115().__ADS1015_REG_CONFIG_MUX_SINGLE_0
         milVolts = ADS1115.ADS1115().readADCSingleEnded(channel=0)
-        pressure = round( milVolts / 5000 * 300, 3)
+        pressure = round(milVolts / 5000 * 300, 3)
         return pressure
 
-    # def readTemperature(self, ads):
-    #    hotJuncVolts = tempVoltage(self, ads)
-    #    coldJuncVolts = coldVoltage(self, ads)
+    def readTemperature(self, ads):
+        hotJuncVolts = tempVoltage(self, ads)
+        # coldJuncVolts = coldVoltage(self, ads)
 
     # Read the voltage difference between pins
-    # def tempVoltage(self, ads):
-    #    pos = ads.readADCSingleEnded(channel=3)
-    #    neg = ads.readADCSingleEnded(channel=2)
-
-    #    return pos - neg
+    def tempVoltage(self, ads):
+        pos = ads.readADCSingleEnded(channel=3)
+        neg = ads.readADCSingleEnded(channel=2)
+        return pos - neg
 
     # def coldVoltage(self, ads):
     # TODO

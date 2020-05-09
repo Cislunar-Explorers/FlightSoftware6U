04046c7d9354e1be9bcb4b590a378b6193c57001
Stefan Brechter <scb262@cornell.edu>
2020-05-09 14:13:14 -0400

    Added temperature

--- drivers/ADCDriver.py
+++ drivers/ADCDriver.py
@@ -54,7 +54,7 @@ class ADC:
     # Read the fuel tank temperature from thermocouple at channels 2 and 3 on the ADS1115
     # Requires a cold junction temperature taken from the Adafruit BNO055 gyroscopic sensor
     def read_temperature(self):
-        hotJuncVolt = self.get_thermocouple_volt()
+        ###hotJuncVolt = self.get_thermocouple_volt()
         cold_junc_temp = self.get_gyro_temp()  # TODO
         # Need cold junction voltage converted from temperature
         cold_junc_volt = self.convert_temp_to_volt(cold_junc_temp)

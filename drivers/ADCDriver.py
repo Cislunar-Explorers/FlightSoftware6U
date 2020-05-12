68e463c127fdaa0163c515c0b880b8e45b0a4bc7
Stefan Brechter <scb262@cornell.edu>
2020-05-12 16:33:27 -0400

    Adding gyro library

--- drivers/ADCDriver.py
+++ drivers/ADCDriver.py
@@ -19,7 +19,7 @@ import time
 class ADC:
     ads = ADS1115.ADS1115()
     i2c = busio.I2C(board.SCL, board.SDA)
-    gyro = adafruit_bno055.BNO055(i2c)
+    gyro = adafruit_bno055.BNO055_I2C(i2c)
 
     # For the thermo couple conversion from voltage to temperature.
     T0 = -8.7935962e0
@@ -51,7 +51,7 @@ class ADC:
     def __init__(self):
         self.ads = ADS1115.ADS1115()
         i2c = busio.I2C(board.SCL, board.SDA)
-        self.gyro = adafruit_bno055.BNO055(i2c)
+        self.gyro = adafruit_bno055.BNO055_I2C(i2c)
 
     # Read the fuel tank pressure from the pressure transducer at channel 0 on the ADS1115
     def read_pressure(self):

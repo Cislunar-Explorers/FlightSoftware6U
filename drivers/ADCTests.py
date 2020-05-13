d44d4ca0a0c20b5a9defdbd8b21dd3d2eab2e346
Stefan Brechter <scb262@cornell.edu>
2020-05-13 12:28:47 -0400

    Adding testing

--- drivers/ADCTests.py
+++ drivers/ADCTests.py
@@ -52,4 +52,4 @@ def test_ADC_read_temperature_20(testADC):
 if True:
     testADC = test_ADC_initialize()
     test_ADC_read_pressure_20(testADC)
-    test_ADC_read_temperature(testADC)
+    # test_ADC_read_temperature(testADC)

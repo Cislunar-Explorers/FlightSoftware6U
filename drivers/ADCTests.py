486e98c555a00d6bb3458a31b94627d7de1c6a24
Stefan Brechter <scb262@cornell.edu>
2020-05-13 12:16:02 -0400

    Adding testing

--- drivers/ADCTests.py
+++ drivers/ADCTests.py
@@ -9,22 +9,46 @@ def test_ADC_initialize():
 
 
 def test_ADC_read_pressure(testADC):
+    print(testADC.read_pressure(testADC))
+
+
+def test_ADC_read_pressure_continuous(testADC):
     while True:
         print(testADC.read_pressure(testADC))
         time.sleep(1)
 
 
+def test_ADC_read_pressure_20(testADC):
+    x = 0
+    while x < 20:
+        test_ADC_read_pressure(testADC)
+        x = x + 1
+        time.sleep(1)
+
+
 def test_ADC_read_temperature(testADC):
+    print("--------------------------")
+    cel = testADC.read_temperature(testADC)
+    print(cel)
+    print("Fahrenheit:")
+    print(cel * 1.8 + 32)
+
+
+def test_ADC_read_temperature_continuous(testADC):
     while True:
-        print("--------------------------")
-        cel = testADC.read_temperature(testADC)
-        print(cel)
-        print("Fahrenheit:")
-        print(cel * 1.8 + 32)
+        test_ADC_read_temperature(testADC)
+        time.sleep(1)
+
+
+def test_ADC_read_temperature_20(testADC):
+    x = 0
+    while x < 20:
+        test_ADC_read_temperature(testADC)
+        x = x + 1
         time.sleep(1)
 
 
 if True:
     testADC = test_ADC_initialize()
-    # test_ADC_read_pressure(testADC)
+    test_ADC_read_pressure_20(testADC)
     test_ADC_read_temperature(testADC)

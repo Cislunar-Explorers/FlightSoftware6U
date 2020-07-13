c6a02e8d2d4a8e65fc4eda9038b2c064c62c6314
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-07-12 21:31:14 -0400

    implement basic logger to log to IO and stdout

---
+++ utils/log.py
@@ -0,0 +1,14 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        RotatingFileHandler("cislunar.log", maxBytes=4096, backupCount=10),
+        logging.StreamHandler(sys.stdout),
+    ]
+)
+
+log = logging.getLogger("CislunarExplorers")

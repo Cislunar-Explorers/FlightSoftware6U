7413e9f909b0e8e4908b538c92ce6e948c0d90ac
Viraj Patel <vdp8@cornell.edu>
2020-07-24 12:02:34 -0400

    added log method

--- tests/time_test.py
+++ tests/time_test.py
@@ -1,4 +1,21 @@
-from FlightSoftware.utils.log import *
+import logging
+from logging.handlers import RotatingFileHandler
+import sys
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] [%(pathname)s:%(filename)s:%(lineno)s] %(message)s",
+    handlers=[
+        RotatingFileHandler("cislunar.log", maxBytes=4096, backupCount=10),
+        logging.StreamHandler(sys.stdout),
+    ]
+)
+
+log = logging.getLogger("CislunarExplorers")
+
+def get_log():
+    return log
+
 import time
 import board
 import datetime

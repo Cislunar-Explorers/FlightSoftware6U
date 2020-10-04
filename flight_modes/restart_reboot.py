7b8f7e83229c4ebc849c2a30bf88d9c03b3c2021
Jonathan Grossman <jag527@cornell.edu>
2020-10-03 20:47:27 -0400

    Update restart_reboot.py

--- flight_modes/restart_reboot.py
+++ flight_modes/restart_reboot.py
@@ -1,63 +1,65 @@
 import time
-
-# os
-# from utils.db import create_sensor_tables_from_path
-# other stuff too?
-
-
+from datetime import datetime
+import os
+from utils.db import create_sensor_tables_from_path 
+from utils.constants import DB_FILE
 
 
-"""
-class BootUpMode( param? ):
+
+
+# the number of times a reboot or bootup has happened
+num_reboots = 0
+
+# dictionary keeping track of number of reboots, and when they happen
+reboots = {}
+
+class BootUpMode:
     def __init__(self):
 
         #do i need to assign any variables? i don't think so but not sure
        
 
         self.wait_a_minute()
-        self.create_database()
-        self.init_spin(this one actually might need some parameters)
+        self.init_spin()
         self.init_add_to_log()
         self.init_camera()
-        self.create_record_of_restarts()
+        self.add_to_restart_records()
 
 
-        self.create_session = create_sensor_tables_from_path( something )
-
+        # create a session... i don't know if the parameter is right here
+        self.create_session = create_sensor_tables_from_path(DB_FILE)
 
 
     def wait_a_minute(self):
         # waits 1 minute before doing anything
         time.sleep(60)
 
-    
-    def create_database(self):
-        # create a database for log entry text files
-        log_entries = []
 
-
-    def init_spin(params):
+    def init_spin(self):
         # make set of thrusts to achieve initial spin
         # i currently have zero clue how to do this
+        pass
 
 
     def init_add_to_log(self):
-        # make initial log entry to log_entries
+        # make initial log entry 
+        pass
 
 
     def init_camera(self):
-        # ask stephen what to do here
         # camera multiplexer needs to be initialized
+        pass
 
 
-    def create_record_of_restarts(self):
-        # creates a blank record (dictionary maybe?)
-        # for future restarts
+    def add_to_restart_records(self):
+        # record that a reboot is happening, and the time it is happening
+        reboots[num_reboots] = datetime.now()
+        num_reboots += 1
 
 
 
 
-classRestartMode( param? ):
+class RestartMode:
     def __init__(self):
 
         #do i need to assign any variables? i don't think so but not sure
@@ -70,16 +72,16 @@ classRestartMode( param? ):
 
 
     def add_to_log(self):
-        # make a log entry text file
+        # make a log entry 
+        pass
 
-
-    def add_to_restart_records(self)
+    def add_to_restart_records(self):
         # record what number restart this is, and maybe the time?
         # add this to the record created in BootUp
+        reboots[num_reboots] = datetime.now()
+        num_reboots += 1
 
 
     def write_to_i2c(self):
         # i have literally no idea what this means
-
-
-"""
+        pass

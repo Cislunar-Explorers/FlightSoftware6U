4345dea8513b13f51f8e2d363e44a0da249551c3
Jonathan Grossman <jag527@cornell.edu>
2020-10-22 16:35:36 -0400

    Some small reformatting

--- flight_modes/restart_reboot.py
+++ flight_modes/restart_reboot.py
@@ -8,12 +8,7 @@ from utils.constants import DB_FILE
 from utils.log import get_log
 from utils.db import RebootsModel
 
-# these imports are necessary for MuxManager (camera init)
-import board
-import busio
-from digitalio import DigitalInOut, Direction
-from adafruit_bus_device.i2c_device import I2CDevice
-from picamera import PiCamera
+
 
 # creates the session
 # am I doing this right? is this session going to remain the same
@@ -53,6 +48,7 @@ class BootUpMode(FlightMode):
     # logger = get_log()
 
     # initialize the cameras, select a camera
+    # CHANGE!
     def init_camera(self):
         # create a MuxManager object
         mux = MuxManager()
@@ -108,52 +104,3 @@ class RestartMode(FlightMode):
 
         # this hopefully should select a camera
         mux.pick()
-
-# this is the MuxManager object
-# created to initailize the cameras
-# this almost entirely comes from mux-test.py
-class MuxManager:
-    def __init__(self):
-
-        self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.mux = I2CDevice(i2c, 0x70)
-
-        self.sel_pin = DigitalInOut(board.D4)
-        self.sel_pin.direction = Direction.OUTPUT
-        self.oe1_pin = DigitalInOut(board.D17)
-        self.oe1_pin.direction = Direction.OUTPUT
-        self.oe2_pin = DigitalInOut(board.D18)
-        self.oe2_pin.direction = Direction.OUTPUT
-
-    # i wrote this, it pretty much tries to pick a camera
-    def pick(self):
-        for id in [1, 3, 4]:
-            selectCamera(id)
-
-    def selectCamera(self, id):
-        if id == 1:
-            with mux:
-                mux.write(bytes([0x04]))
-            sel_pin.value = False
-            oe1_pin.value = False
-            oe2_pin.value = True
-        elif id == 2:
-            with mux:
-                mux.write(bytes([0x05]))
-            sel_pin.value = True
-            oe1_pin.value = False
-            oe2_pin.value = True
-        elif id == 3:
-            with mux:
-                mux.write(bytes([0x06]))
-            sel_pin.value = False
-            oe1_pin.value = True
-            oe2_pin.value = False
-        elif id == 4:
-            with mux:
-                mux.write(bytes([0x07]))
-            sel_pin.value = True
-            oe1_pin.value = True
-            oe2_pin.value = False
-        else:
-            assert False

73b214d90f623eb48419d719385f99014404a3e9
Jonathan Grossman <jag527@cornell.edu>
2020-10-22 15:28:19 -0400

    Some small reformatting

--- flight_modes/restart_reboot.py
+++ flight_modes/restart_reboot.py
@@ -2,7 +2,8 @@ import time
 from datetime import datetime
 import os
 import sys
-from utils.db import create_sensor_tables_from_path 
+
+from utils.db import create_sensor_tables_from_path
 from utils.constants import DB_FILE
 from utils.log import get_log
 from utils.db import RebootsModel
@@ -14,19 +15,16 @@ from digitalio import DigitalInOut, Direction
 from adafruit_bus_device.i2c_device import I2CDevice
 from picamera import PiCamera
 
-
-
-
 # creates the session
-    # am I doing this right? is this session going to remain the same
-    #... for the entire flight? i want to make sure it's adding to
-    #... the same session always
+# am I doing this right? is this session going to remain the same
+# ... for the entire flight? i want to make sure it's adding to
+# ... the same session always
 create_session = create_sensor_tables_from_path(DB_FILE)
 session = create_session()
 
+
 class BootUpMode(FlightMode):
     def __init__(self, parent):
-
         super().__init__(parent)
 
         # wait a minute
@@ -45,43 +43,39 @@ class BootUpMode(FlightMode):
         # initialize the camera
         self.init_camera()
 
-
-# TODO make set of initial thrusts to achieve spin
+    # TODO make set of initial thrusts to achieve spin
     def init_spin(self):
         pass
 
-
-    # ignore this for now, idk if it is necessary or what it is 
+    # ignore this for now, idk if it is necessary or what it is
     # def init_add_to_log(self):
-        # make initial log entry
-        # logger = get_log() 
-
+    # make initial log entry
+    # logger = get_log()
 
     # initialize the cameras, select a camera
     def init_camera(self):
         # create a MuxManager object
         mux = MuxManager()
-        
+
         # this hopefully should select a camera
-        mux.pick() 
+        mux.pick()
 
+        # this should add to the RebootsModel
 
-    # this should add to the RebootsModel
     # will add info about the bootup to the db
     def log(self):
         type_of_reboot = "Bootup"
         reboot_at = datetime.now()
         new_bootup = RebootsModel(type_of_reboot=type_of_reboot,
-        reboot_at=reboot_at)
+                                  reboot_at=reboot_at)
         session.add(new_bootup)
         session.commit()
 
 
 class RestartMode(FlightMode):
     def __init__(self, parent):
-
         super().__init__(parent)
-   
+
         # add info about restart to database
         self.log()
 
@@ -92,12 +86,10 @@ class RestartMode(FlightMode):
         # double check that the cameras are good
         self.init_camera()
 
-
     # TODO
     def add_to_log(self):
         # make a log entry 
         logger = get_log()
-
 
     # this should add to the RebootsModel
     # will add info about the bootup to the db
@@ -105,21 +97,21 @@ class RestartMode(FlightMode):
         type_of_reboot = "Restart"
         reboot_at = datetime.now()
         new_bootup = RebootsModel(type_of_reboot=type_of_reboot,
-        reboot_at=reboot_at)
+                                  reboot_at=reboot_at)
         session.add(new_bootup)
         session.commit()
-
 
     # initialize the cameras, select a camera
     def init_camera(self):
         # create a MuxManager object
         mux = MuxManager()
-        
+
         # this hopefully should select a camera
-        mux.pick() 
+        mux.pick()
+
+    # this is the MuxManager object
 
 
-# this is the MuxManager object
 # created to initailize the cameras
 # this almost entirely comes from mux-test.py
 class MuxManager:
@@ -139,7 +131,6 @@ class MuxManager:
     def pick(self):
         for id in [1, 3, 4]:
             selectCamera(id)
-
 
     def selectCamera(self, id):
         if id == 1:
@@ -166,7 +157,5 @@ class MuxManager:
             sel_pin.value = True
             oe1_pin.value = True
             oe2_pin.value = False
-        else: assert False
-
-
-
+        else:
+            assert False

a85460aad6b3bab77982545b8f6d13d414bb8cf9
Stephen Zakoworotny <54723402+sjz38@users.noreply.github.com>
2020-11-21 09:30:07 -0500

    Add self to rawObservation

--- OpticalNavigation/core/camera.py
+++ OpticalNavigation/core/camera.py
@@ -76,7 +76,7 @@ class Camera:
 
 
     # Code from video-timing.py
-    def rawObservation(filename, frame_rate=15, shutterSpeed = 30000):
+    def rawObservation(self, filename, frame_rate=15, shutterSpeed = 30000):
 
         # Analog and digital gain parameters (not exposed in picamera-1.13)
         MMAL_PARAMETER_ANALOG_GAIN = mmal.MMAL_PARAMETER_GROUP_CAMERA + 0x59

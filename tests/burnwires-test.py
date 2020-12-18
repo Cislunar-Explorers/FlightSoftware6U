b143dee1eed50b489b1eab5c3ca76e43ae4207d3
Toby <fischer.m.tobias@gmail.com>
2020-12-17 21:23:02 -0800

    burnwires-test.py now reads gyro data from new thread

--- tests/burnwires-test.py
+++ tests/burnwires-test.py
@@ -2,33 +2,36 @@ from drivers.gom import Gomspace
 from datetime import datetime
 from utils.log import get_log
 from drivers.gyro import GyroSensor
+from time import sleep
+import threading
 
-# from utils.constants import
 
-logger = get_log()
-gyro = GyroSensor()
-gom_controller = Gomspace()
+# from utils.constants import BURNWIRE_DURATION
 
-gom_controller.burnwire2(1, delay=5)
-start_time = datetime.now()
 
-difference = 0
-gyro_data = []
-logger.info("Gyro data (rad/s)")
-while difference < 20:
-    difference = (datetime.now() - start_time).total_seconds()
-    # read gom output data
-    hkdata = gom_controller.get_health_data("eps")
-
-    # read gyro data
-    for i in range(10):
+def gyro_thread():
+    gyro_data = []
+    logger.info("Reading Gyro data (rad/s)")
+    for i in range(100):
         gyro_reading = gyro.get_gyro()
         gyro_time = datetime.now().time().isoformat()
         gyro_list = list(gyro_reading)
         gyro_list.append(gyro_time)
         gyro_data.append(gyro_list)
-        logger.info(f"{gyro_reading}")
+        sleep(0.05)
 
-# writes gyro data to gyro_data.txt. Caution, this file will be overwritten with every successive test
-with open('gyro_data.txt', 'w') as filehandle:
-    filehandle.writelines("%s\n" % line for line in gyro_data)
+    # writes gyro data to gyro_data.txt. Caution, this file will be overwritten with every successive test
+    logger.info("Writing gyro data to file")
+    with open('gyro_data.txt', 'w') as filehandle:
+        filehandle.writelines("%s\n" % line for line in gyro_data)
+
+
+if __name__ == '__main__':
+    logger = get_log()
+    gyro = GyroSensor()
+    gom_controller = Gomspace()
+
+    # start new thread to log gyro data
+    gyro_threader = threading.Thread(target=gyro_thread)
+    gyro_threader.start()
+    gom_controller.burnwire2(2)

497499a9b0035eb86e05e29ca0c84a7291d9fdc4
Jonathan Grossman <jag527@cornell.edu>
2021-01-21 17:31:35 -0500

    -fake opnav mode in place -testing from normal --> opnav --> normal

--- flight_modes/restart_reboot.py
+++ flight_modes/restart_reboot.py
@@ -46,7 +46,7 @@ class BootUpMode(FlightMode):
 
     def log(self):
         is_bootup = True
-        reboot_at = psutil.boot_time()
+        reboot_at = datetime.fromtimestamp(psutil.boot_time())
         new_bootup = RebootsModel(is_bootup=is_bootup,
                                   reboot_at=reboot_at)
         self.session.add(new_bootup)

796d6bc4d7a4f4150f57afed323bf5b28756fcb8
alexbuzali2233 <61602738+alexbuzali2233@users.noreply.github.com>
2021-02-06 13:25:38 -0500

    Comms Security Test

--- tests/comms_security_test.py
+++ tests/comms_security_test.py
@@ -8,12 +8,12 @@ ch = CommandHandler()
 groundstation = Radio()
 
 #Invalid counter
-badCounterCommand = ch.pack_command(2,8,7)
-groundstation.transmit(badCounterCommand)
+#badCounterCommand = ch.pack_command(2,8,7)
+#groundstation.transmit(badCounterCommand)
 
 #Invalid MAC
-#invalidMAC = ch.pack_command(1,8,7)
-#groundstation.transmit(badCounterCommand)
+invalidMAC = ch.pack_command(1,8,7)
+groundstation.transmit(invalidMAC)
 
 #Shutdown via command
 #shutdown = ch.pack_command(1,8,11)

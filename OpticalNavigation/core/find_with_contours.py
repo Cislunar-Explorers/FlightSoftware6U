0ce25120937aa1c687faeff5be3428a2a6209ca7
Stephen Zakoworotny <54723402+sjz38@users.noreply.github.com>
2021-02-16 21:15:56 -0500

    Find module updates

--- OpticalNavigation/core/find_with_contours.py
+++ OpticalNavigation/core/find_with_contours.py
@@ -4,8 +4,9 @@ import numpy as np
 from math import pi, radians, tan, floor, ceil
 import os
 import copy
+import argparse
 
-from core.find_with_kmeans import getkmeans
+#from core.find_with_kmeans import getkmeans
 
 class Camera:
     """Encapsulate field-of-view and resolution of a camera."""
@@ -205,97 +206,190 @@ def bufferedRoi(x, y, w, h, wTot, hTot, b):
     yr = min(y+h+b, hTot)
     return (xl, yl, xr-xl, yr-yl)
 
+def measureEarth(img):
+		lowThresh = cv2.inRange(img, (75, 50, 0), (255, 230, 100))
+		percentWhite = cv2.countNonZero(lowThresh) / (lowThresh.shape[0] * lowThresh.shape[1])
+		print(percentWhite)
+		if percentWhite >= 0.23:
+			highThresh = cv2.inRange(img, (75, 50, 0), (255, 230, 180))
+			cv2.imshow("Earth thresh", highThresh)
+			#bw = cv2.threshold(img[:,:,1], thresh, 255, cv2.THRESH_BINARY)[1]
+			contours = cv2.findContours(highThresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+			contours = contours[0] if len(contours) == 2 else contours[1]
+	
+			#for c in contours:
+			c = contours[0]
+			xy, (major, minor), ang = cv2.minAreaRect(c)
+			r = major/2
+			# TODO: Shift center based on aspect ratio, center of "mass"
+			return xy, r
+		else:
+			return None
+
 # Measure blue channel (least sensitive), assume full disk
 def measureSun(img):
-    thresh = 245
-    bw = cv2.threshold(img[:,:,0], thresh, 255, cv2.THRESH_BINARY)[1]
-    contours = cv2.findContours(bw, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = contours[0] if len(contours) == 2 else contours[1]
-
-    #for c in contours:
-    c = contours[0]
-    xy, (major, minor), _ = cv2.fitEllipse(c)
-    r = (major + minor)/4
-    return xy, r
+		highThresh = cv2.inRange(img, (230, 230, 230), (255, 255, 255))
+		percentWhite = cv2.countNonZero(highThresh) / (highThresh.shape[0] * highThresh.shape[1])
+		print(percentWhite)
+		if percentWhite >= 0.23:	
+			#thresh = 245
+			#bw = cv2.threshold(img[:,:,0], thresh, 255, cv2.THRESH_BINARY)[1]
+			contours = cv2.findContours(highThresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+			contours = contours[0] if len(contours) == 2 else contours[1]
+	
+			#for c in contours:
+			c = contours[0]
+			xy, (major, minor), _ = cv2.fitEllipse(c)
+			r = (major + minor)/4
+			return xy, r
+		else:
+			return None
 
 # Measure green channel (alternatively, value)
 # TODO: Why only green channel?
 def measureMoon(img):
-    thresh = 128
-    bw = cv2.threshold(img[:,:,1], thresh, 255, cv2.THRESH_BINARY)[1]
-    contours = cv2.findContours(bw, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours = contours[0] if len(contours) == 2 else contours[1]
+		thresh = cv2.inRange(img, (50, 50, 50), (255, 255, 255))
+		#thresh = 128
+		#bw = cv2.threshold(img[:,:,1], thresh, 255, cv2.THRESH_BINARY)[1]
+		contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+		contours = contours[0] if len(contours) == 2 else contours[1]
+		if contours is not None:
+			#for c in contours:
+			c = contours[0]
+			xy, (major, minor), ang = cv2.minAreaRect(c)
+			r = major/2
+			# TODO: Shift center based on aspect ratio, center of "mass"
+			return xy, r
+		else:
+			return None
 
-    #for c in contours:
-    c = contours[0]
-    xy, (major, minor), ang = cv2.minAreaRect(c)
-    r = major/2
-    # TODO: Shift center based on aspect ratio, center of "mass"
-    return xy, r
 
-
-cam = Camera(radians(62.2), radians(48.8), 3280, 2464)
-u = np.array([0, 1, 0], dtype=np.float32)
-#omega = 78/60 * 2 * pi
-omega = 0
-dt = 18.904e-6
-rot = CameraRotation(u, -omega*dt)
-
-#img = cv2.imread('sun1.jpg')
-img = cv2.imread(os.path.join("D:", "OpNav", "data", "EclipseAndCrescentImages", "images", "EarthMoon.jpg"))
-imgCopy = img.copy()
-
-# In-place blur to reduce noise, avoid hot pixels
-img = cv2.GaussianBlur(img, (5,5), 0, dst=img)
-
-# Extract and threshold blue channel (least sensitive)
-thresh = 64 # 245
-bw = cv2.threshold(img[:,:,0], thresh, 255, cv2.THRESH_BINARY)[1]
-cv2.imshow("Blue channel threshold", bw)
-
-# bw = getkmeans(img)
-# thresh = 10 # 245
-# bw = cv2.threshold(img[:,:,0], thresh, 255, cv2.THRESH_BINARY)[1]
-# bw = cv2.medianBlur(bw, 51)
-# cv2.imshow("Segmented", bw)
-# cv2.waitKey(0)
-
-contours = cv2.findContours(bw, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-# Hack around API breakage between OpenCV versions
-contours = contours[0] if len(contours) == 2 else contours[1]
-
-cv2.drawContours(imgCopy, contours, -1, (0,255,0), 3)
-cv2.imshow("Contours", imgCopy)
-
-#for c in contours:
-c = contours[0]
-x, y, w, h = cv2.boundingRect(c)
-x, y, w, h = bufferedRoi(x, y, w, h, cam.w, cam.h, 16)
-src = BoundingBox(x, y, w, h)
-
-#src = BoundingBox(0, 0, cam.w, cam.h)
-out, bbst = remap_roi(img, src, cam, rot)
-
-# Measure body in region-of-interest
-#(x, y), r = measureSun(out)
-(x, y), r = measureMoon(out)
-
-# Shift stereographic coordinates of center to camera frame (at start of exposure)
-xst, yst = cam.normalize_st(bbst.x0 + x, bbst.y0 + y)
-
-# Adjust diameter for stereographic distortion
-rho2 = xst**2 + yst**2
-dia = 4*2*r*(2*cam.xmax_st/cam.w)/(4 + rho2)
-
-# Compute spherical coordinates of center (in camera frame at start of exposure)
-sx, sy, sz = st_to_sph(xst, yst)
-
-# Location of circle center in spherical coordinates,
-# Angular diameter of circle in radians
-print(sx, sy, sz, dia)
-
-cv2.waitKey(0)
+def find(src):
+	cam = Camera(radians(62.2), radians(48.8), 3280, 2464)
+	u = np.array([0, 1, 0], dtype=np.float32)
+	#omega = 78/60 * 2 * pi
+	#omega = 6
+	omega = 0
+	dt = 18.904e-6
+	rot = CameraRotation(u, -omega*dt)
+	
+	#img = cv2.imread('sun1.jpg')
+	img = cv2.imread(src)
+	imgCopy = img.copy()
+	
+	# In-place blur to reduce noise, avoid hot pixels
+	img = cv2.GaussianBlur(img, (5,5), 0, dst=img)
+	
+	
+	# Extract and threshold blue channel (least sensitive)
+	thresh = 64 # 245
+	bw = cv2.threshold(img[:,:,0], thresh, 255, cv2.THRESH_BINARY)[1]
+	cv2.imshow("Blue channel threshold", bw)
+	
+	# bw = getkmeans(img)
+	# thresh = 10 # 245
+	# bw = cv2.threshold(img[:,:,0], thresh, 255, cv2.THRESH_BINARY)[1]
+	# bw = cv2.medianBlur(bw, 51)
+	# cv2.imshow("Segmented", bw)
+	# cv2.waitKey(0)
+	
+	contours = cv2.findContours(bw, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+	
+	# Hack around API breakage between OpenCV versions
+	contours = contours[0] if len(contours) == 2 else contours[1]
+	
+	cv2.drawContours(imgCopy, contours, -1, (0,255,0), 3)
+	cv2.imshow("Contours", imgCopy)
+	
+	#for c in contours:
+	#c = contours[0]
+	areas = [cv2.contourArea(c) for c in contours] 
+	max_index = np.argmax(areas)
+	c = contours[max_index]
+	#print(len(contours))
+	x, y, w, h = cv2.boundingRect(c)
+	#print(x, y, w, h)
+	full_contour = cv2.rectangle(imgCopy, (x,y),(x+w,y+h),(0,255,0),2)
+	cv2.imshow("full_contoru", full_contour)
+	x, y, w, h = bufferedRoi(x, y, w, h, cam.w, cam.h, 16)
+	#print(x, y, w, h)
+	src = BoundingBox(x, y, w, h)
+	
+	#src = BoundingBox(0, 0, cam.w, cam.h)
+	out, bbst = remap_roi(img, src, cam, rot)
+	
+	##
+	cv2.imshow("roi", out)
+	##
+	sun = None
+	moon = None
+	
+	# Measure body in region-of-interest
+	earth = measureEarth(out)
+	if earth is not None:
+		(eX, eY), eR = earth
+		eXst, eYst = cam.normalize_st(bbst.x0 + eX, bbst.y0 + eY)
+		eRho2 = eXst**2 + eYst**2
+		eDia = 4*2*eR*(2*cam.xmax_st/cam.w)/(4 + eRho2)
+		eSx, eSy, eSz = st_to_sph(eXst, eYst)
+		print("Earth")
+		print(eSx, eSy, eSz, eDia)
+	if earth is None:
+		sun = measureSun(out)
+		if sun is not None:
+			(sX, sY), sR = sun
+			sXst, sYst = cam.normalize_st(bbst.x0 + sX, bbst.y0 + sY)
+			sRho2 = sXst**2 + sYst**2
+			sDia = 4*2*sR*(2*cam.xmax_st/cam.w)/(4 + sRho2)
+			sSx, sSy, sSz = st_to_sph(sXst, sYst)
+			print("Sun")
+			print(sSx, sSy, sSz, sDia)
+		if sun is None:
+			moon = measureMoon(out)
+			if moon is not None:
+				(mX, mY), mR = moon
+				mXst, mYst = cam.normalize_st(bbst.x0 + mX, bbst.y0 + mY)
+				mRho2 = mXst**2 + mYst**2
+				mDia = 4*2*mR*(2*cam.xmax_st/cam.w)/(4 + mRho2)
+				mSx, mSy, mSz = st_to_sph(mXst, mYst)
+				print("Moon")
+				print(mSx, mSy, mSz, mDia)
+		
+	
+	
+	# Shift stereographic coordinates of center to camera frame (at start of exposure)
+	
+	# Adjust diameter for stereographic distortion
+	
+	# Compute spherical coordinates of center (in camera frame at start of exposure)
+	
+	# Location of circle center in spherical coordinates,
+	# Angular diameter of circle in radians
+	
+	
+	
+	cv2.waitKey(0)
+	
+# Notes
+# * Need sanity check on contour size (not too large, not too small)
+# * Need sanity check on omega (too high and out will be too big)
+# * Need to characterize, avoid sun flare in planet exposures
+# * Sun is still too bright even with min exposure
+#   * Can white balance, gamma reduce sensitivity?
+#   * Can we install ND filters (and how would they affect planet exposures)?
+#   * Note: Can measure center quite well, just not size; is it required?  Could we fudge it with a correction factor?
+# * For Moon, blurry edges and non-uniform surface make picking threshold value difficult
+#   * Consider analyzing ROI of original, unblurred image
+# * Bottleneck on RPi appears to be finding contours on full-res image.  Consider finding contours on low-res image, then scaling ROI
+if __name__ == "__main__":
+    """
+    Run "python3 find_with_contours.py -i=<IMAGE>" to test this module
+    """
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", help = "path to the image")
+    args = vars(ap.parse_args())
+    
+    find(args["image"])
 
 # Notes
 # * Need sanity check on contour size (not too large, not too small)
@@ -308,3 +402,4 @@ cv2.waitKey(0)
 # * For Moon, blurry edges and non-uniform surface make picking threshold value difficult
 #   * Consider analyzing ROI of original, unblurred image
 # * Bottleneck on RPi appears to be finding contours on full-res image.  Consider finding contours on low-res image, then scaling ROI
+	

5e59efd7f5cd53db6e4f6b585f04a66a0b2a14cb
alexbuzali2233 <61602738+alexbuzali2233@users.noreply.github.com>
2021-03-23 18:49:52 -0400

    File Upload Testing/Users/alexbuzali/opt/anaconda3/bin/python

--- HITL-tests/file_transmitter.py
+++ HITL-tests/file_transmitter.py
@@ -1,10 +1,10 @@
 from utils.constants import MIN_COMMAND_SIZE, FMEnum, CommandCommandEnum, FLIGHT_SOFTWARE_PATH
-from communications.satellite_radio import Radio
+#from communications.satellite_radio import Radio
 from communications.commands import CommandHandler
 import hashlib
 import time
 
-groundstation = Radio()
+#groundstation = Radio()
 ch = CommandHandler()
 command_counter = 1
 transmission_interval = 4
@@ -12,7 +12,8 @@ file_path_for_opening = FLIGHT_SOFTWARE_PATH+'FlightSoftware/HITL-tests/test_upl
 file_path = 'HITL-tests/test_upload_file.py'
 
 #Get file
-max_string_size = 195 - 50 - 2 - MIN_COMMAND_SIZE
+#Max transmission size - space alotted for file name - block number - min command size
+max_string_size = 195 - 50 - 2 - MIN_COMMAND_SIZE - 4
 file = open(file_path_for_opening)
 file_string = file.read()
 file_blocks = []
@@ -33,6 +34,8 @@ for block in file_blocks:
     CommandCommandEnum.AddFileBlock.value, file_path = file_path,
     block_number = block[0],block_text = block[1])
 
-    groundstation.transmit(block_command)
+    print(ch.unpack_command(block_command))
+
+    """groundstation.transmit(block_command)
     print('Transmitted Block #' + str(block[0]))
-    time.sleep(transmission_interval)
+    time.sleep(transmission_interval)"""

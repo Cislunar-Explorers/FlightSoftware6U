4fca4cca6dfcf9bde6020f096dde5e4170f8f6a6
alexbuzali2233 <61602738+alexbuzali2233@users.noreply.github.com>
2021-03-23 20:59:30 -0400

    File Upload Testing

--- communications/command_definitions.py
+++ communications/command_definitions.py
@@ -501,6 +501,7 @@ class CommandDefinitions:
             full_file_text += self.parent.file_block_bank[i][1]
 
         file_already_exists = os.path.exists(file_path)
+        print(file_already_exists)
 
         #Opens target file, creates one with the given path if it doesn't exist yet
         original_file = open(file_path, 'w')
@@ -509,6 +510,7 @@ class CommandDefinitions:
         if file_already_exists:
             original_file = open(file_path, 'r+')
             original_file_lines = original_file.readlines()
+            print(original_file_lines)
             backup_name = FLIGHT_SOFTWARE_PATH + local_file_name[:local_file_name.index('.py')] + '_backup.py'
             backup_file = open(backup_name, 'w')
             backup_file.writelines(original_file_lines)

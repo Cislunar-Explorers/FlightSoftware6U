7c70426412b62020d96d442a82c478b9e669ddc2
tmf97 <tmf97@cornell.edu>
2021-03-27 16:01:55 -0400

    Put burnwire into seconds
    Signed-off-by: tmf97 <tmf97@cornell.edu>

--- HITL-tests/gom_console.py
+++ HITL-tests/gom_console.py
@@ -25,15 +25,18 @@ while True:
     if choice == 0:
         ps.displayHk2(gom.get_health_data(level='eps'))
 
-    if choice in [1, 6, 7]:
+    if choice in [1, 4, 6, 7]:
         duration = int(input("Duration (integer seconds):\n"))
-        assert 0 < duration < 10
+        assert 0 < duration < 15
         if choice == 1:
             gom.lna(True)
             sleep(duration / 2)
             ps.displayHk2(gom.get_health_data(level='eps'))
             sleep(duration / 2)
             gom.lna(False)
+
+        if choice == 4:
+            gom.burnwire1(duration)
 
         if choice == 6:
             gom.set_electrolysis(True)
@@ -49,7 +52,7 @@ while True:
             sleep(duration / 2)
             gom.set_PA(False)
 
-    if choice in [2, 3, 4, 5]:
+    if choice in [2, 3, 5]:
         duration = int(input("Duration (integer milliseconds):\n"))
         if choice == 2:
             gom.pc.set_single_output("glowplug_2", 1, 0)
@@ -62,9 +65,6 @@ while True:
             ps.displayHk2(gom.get_health_data(level="eps"))
             sleep(1e-3 * duration)
             gom.pc.set_single_output("glowplug", 0, 0)
-
-        if choice == 4:
-            gom.burnwire1(duration)
 
         if choice == 5:
             assert 0 < duration < 300

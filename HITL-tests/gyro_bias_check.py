f807396252d1e13b9e9cfda651f2bfd176af171b
tmf97 <tmf97@cornell.edu>
2021-04-09 16:34:18 -0400

    Add bias and temperature correction factors in new get_corrected_bias method, and relevant entries in parameters.py
    Signed-off-by: tmf97 <tmf97@cornell.edu>

--- HITL-tests/gyro_bias_check.py
+++ HITL-tests/gyro_bias_check.py
@@ -3,13 +3,18 @@ import numpy as np
 
 gyro = GyroSensor()
 
-# take 1000 gyro measurements and store data
+# take 500 gyro measurements and store data
 
-data = [] * 1000
+data = [] * 500
 
+temperature = gyro.get_temp()
 for i in range(len(data)):
     data[i] = gyro.get_gyro()
 
 data = np.array(data)
 biases = np.mean(data, axis=0)
 standard_devs = np.std(data, axis=0)
+
+print(f"Biases: {biases}")
+print(f"Stds: {standard_devs}")
+print(f"Temperature: {temperature}")
--- drivers/gyro.py
+++ drivers/gyro.py
@@ -5,9 +5,16 @@ import adafruit_fxos8700
 import adafruit_fxas21002c
 from typing import Tuple
 from utils.constants import GYRO_RANGE
+import utils.parameters as params
+
+gyro_biases = (params.GYRO_BIAS_X, params.GYRO_BIAS_Y, params.GYRO_BIAS_Z)
+gyro_biases_temperature_dependence = (params.GYRO_BIAS_DXDT,
+                                      params.GYRO_BIAS_DYDT,
+                                      params.GYRO_BIAS_DZDT)
 
 
 class GyroSensor:  # TODO rename class and file to something more representative
+
     def __init__(self):
         i2c = busio.I2C(board.SCL, board.SDA)
         self.fxos = adafruit_fxos8700.FXOS8700(i2c)
@@ -18,6 +25,17 @@ class GyroSensor:  # TODO rename class and file to something more representative
 
     def get_gyro(self) -> Tuple[float, float, float]:
         return self.fxas.gyroscope  # rad/s
+
+    def get_gyro_corrected(self):
+        """Returns gyro data with corrections for measured bias"""
+        gyro_data = self.get_gyro()
+        delta_gyro_temp = self.get_temp() - params.GYRO_BIAS_TEMPERATURE
+        corrected_gyro_data = [0.0, 0.0, 0.0]
+        for i in range(3):
+            corrected_gyro_data[i] = gyro_data[i] - gyro_biases[i]
+            corrected_gyro_data[i] = corrected_gyro_data[i] + gyro_biases_temperature_dependence[i] * delta_gyro_temp
+
+        return tuple(corrected_gyro_data)
 
     def get_mag(self) -> Tuple[float, float, float]:
         return self.fxos.magnetometer  # microTeslas

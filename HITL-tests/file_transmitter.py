4990eed7be6a9545c3108504ece0694e5ea18e43
alexbuzali2233 <61602738+alexbuzali2233@users.noreply.github.com>
2021-04-14 20:10:23 -0400

    File Upload/Command Return Update

--- HITL-tests/file_transmitter.py
+++ HITL-tests/file_transmitter.py
@@ -1,29 +1,28 @@
 from utils.constants import MIN_COMMAND_SIZE, FMEnum, CommandCommandEnum, FLIGHT_SOFTWARE_PATH
-#from communications.satellite_radio import Radio
+from communications.satellite_radio import Radio
 from communications.commands import CommandHandler
 from communications.downlink import DownlinkHandler
 import hashlib
 import time
 
-#groundstation = Radio()
+groundstation = Radio()
 ch = CommandHandler()
 command_counter = 1
 transmission_interval = 4
-#file_path = FLIGHT_SOFTWARE_PATH + 'FlightSoftware/communications/command_definitions.py'
+file_path_for_opening = FLIGHT_SOFTWARE_PATH + 'FlightSoftware/HITL-tests/test_file'
+
+print('File Checksum: ' + str(hashlib.md5(full_file_text.encode('utf-8')).hexdigest()))
 
 #Get file
 #Max transmission size - space alotted for file name - block number - 
-# min command size - (2 bytes for string length)
-max_string_size = 195 - MIN_COMMAND_SIZE - 4
-file = open(file_path)
+# min command size - 2*(2 bytes for string length)
+max_string_size = 195 - MIN_COMMAND_SIZE
+file = open(file_path_for_opening)
 file_string = file.read()
 file_blocks = []
 
-print('File Checksum: ' + str(hashlib.md5(file_string.encode('utf-8')).hexdigest()))
-
 #Determine number of blocks
 number_of_blocks = len(file_string)//max_string_size
-print(number_of_blocks)
 if len(file_string) % max_string_size != 0:
     number_of_blocks += 1
 
@@ -36,7 +35,7 @@ i = 0
 for block in file_blocks:
     
     block_command = ch.pack_command(command_counter, FMEnum.Command.value, 
-    CommandCommandEnum.AddFileBlock.value,
+    CommandCommandEnum.AddFileBlock.value, file_path = file_path,
     block_number = block[0],block_text = block[1])
 
     groundstation.transmit(block_command)

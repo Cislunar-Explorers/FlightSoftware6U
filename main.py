52e1968925eed20250b0422b07ce2ff338f8a491
alexbuzali2233 <61602738+alexbuzali2233@users.noreply.github.com>
2021-04-14 20:38:00 -0400

    File Upload/Command Return Update

--- main.py
+++ main.py
@@ -308,6 +308,7 @@ class MainSatelliteThread(Thread):
                 #sleep(5)  # TODO remove when flight modes execute real tasks
                 self.poll_inputs()
                 self.update_state()
+                print('hi')
                 self.read_command_queue_from_file()
                 self.execute_commands()  # Set goal or execute command immediately
                 self.run_mode()

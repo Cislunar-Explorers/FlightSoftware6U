8411e1dfb46ec68e3fcc082fd8ed70452fadfc74
tmf97 <tmf97@cornell.edu>
2021-04-17 12:13:17 -0400

    Fix gyro bias script
    Signed-off-by: tmf97 <tmf97@cornell.edu>

--- HITL-tests/gyro_bias_check.py
+++ HITL-tests/gyro_bias_check.py
@@ -7,13 +7,18 @@ gyro = GyroSensor()
 
 # take 500 gyro measurements and store data
 
-data = [] * 500
-
+data = [None] * 500
+# print("getting data")
 temperature = gyro.get_temp()
 for i in range(len(data)):
     data[i] = gyro.get_gyro()
 
+# print("Done getting data")
+
 data = np.array(data)
+
+data = np.array(data)
+
 biases = np.mean(data, axis=0)
 standard_devs = np.std(data, axis=0)
 

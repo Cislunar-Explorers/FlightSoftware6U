cce9fd7bb41d6d7b3ad0a35fcf771baf25d2e37f
tmf97 <tmf97@cornell.edu>
2021-04-19 09:11:05 -0400

    Filled in values for bias and std from HITL test
    Signed-off-by: tmf97 <tmf97@cornell.edu>

--- drivers/gyro.py
+++ drivers/gyro.py
@@ -42,24 +42,3 @@ class GyroSensor:  # TODO rename class and file to something more representative
 
     def get_temp(self) -> float:
         return self.fxas._read_u8(0x12)
-
-
-# Test to estimate the constant bias for the gyroscopic measurements
-def estimate_constant_bias():
-    tuple_sum = [0.0, 0.0, 0.0]
-
-    for i in range(1000):
-        gyro_val = sensor.gyro
-        tuple_sum[0] += gyro_val[0]
-        tuple_sum[1] += gyro_val[1]
-        tuple_sum[2] += gyro_val[2]
-        time.sleep(1)
-
-    print(f"Tuple sum: {tuple_sum}")
-    tuple_avg = [(val / 1000.0) for val in tuple_sum]
-    print(f"Average gyro values: {tuple_avg}")
-
-
-# TODO
-def estimate_white_noise():
-    pass

a0ef6c7c909a1f548adf540f4bcebf44503134d7
tmf97 <tmf97@cornell.edu>
2021-08-15 19:42:16 -0700

    Add more assert statements into main_test.py
    Signed-off-by: tmf97 <tmf97@cornell.edu>

--- tests/main_test.py
+++ tests/main_test.py
@@ -8,6 +8,18 @@ class MyTestCase(unittest.TestCase):
         self.assertIsNotNone(m.flight_mode)
         self.assertIsNotNone(m.FMQueue)
         self.assertIsNotNone(m.telemetry)
+        self.assertIsNotNone(m.command_definitions)
+        self.assertIsNotNone(m.command_handler)
+
+        # Assert all sensors are None
+        self.assertIsNone(m.gom)
+        self.assertIsNone(m.adc)
+        self.assertIsNone(m.gyro)
+        self.assertIsNone(m.rtc)
+        self.assertIsNone(m.radio)
+        self.assertIsNone(m.mux)
+        self.assertIsNone(m.camera)
+        self.assertIsNone(m.nemo_manager)
 
 
 if __name__ == '__main__':

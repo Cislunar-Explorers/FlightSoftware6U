e066642a1101cbb9281cabb36ae5f33c99d01e72
kahyunnam <69875166+kahyunnam@users.noreply.github.com>
2021-10-16 15:59:55 -0400

    main

--- GroundStation/main.py
+++ GroundStation/main.py
@@ -2,4 +2,4 @@ from udp_client_tester.servertester import ServerTester
 
 if __name__ == "__main__":
     server = ServerTester("192.168.0.101", 3333)
-    server.listen_for_data
+    server.listen_for_data()

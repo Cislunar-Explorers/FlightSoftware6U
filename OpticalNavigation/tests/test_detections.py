f568bafa580d48782bc27285885b170f0b866e0c
Stephen Zakoworotny <sjz38@cornell.edu>
2021-10-28 11:19:58 -0400

    Detection test done

--- OpticalNavigation/tests/test_detections.py
+++ OpticalNavigation/tests/test_detections.py
@@ -5,7 +5,10 @@ import glob
 import os
 
 class Detections(unittest.TestCase):
-    
+    """Tests the detection acciracy of a given find algorithm. This tests whether we are able to 
+    detect the presence of the earth, moon, or sun in a set of images. The truth data is hand analyzed
+    and the data is populated in a csv file."""
+
     # Very similar to function definition of the same name in opnav.py
     def get_detections(self, frames):
         #These arrays take the form (number if frame number): [[x0,y0,z0,diameter0], [x1,y1,z1,diameter1], ...]
@@ -21,16 +24,8 @@ class Detections(unittest.TestCase):
             progress += 1
         return earthDetectionArray, moonDetectionArray, sunDetectionArray    
     
-    
-    
+    # Test to get detection accuracy
     def test_get_truth_detections(self):
-#        with open('traj.csv') as f:
-#            for line in f:
-#                tokens = [float(t) for t in line.split(',')]
-#                t0 = tokens[0]
-#                moonPos = vec3(tokens[4], tokens[5], tokens[6])
-#                sunPos = vec3(tokens[7], tokens[8], tokens[9])
-#                cameraPos = vec3(tokens[1], tokens[2], tokens[3])
         filenames = []
         earthDetect = []
         moonDetect = []
@@ -40,12 +35,13 @@ class Detections(unittest.TestCase):
         with open('/home/stephen/Desktop/truth_detections_case1c.csv') as f:
             for line in f:
                 tokens = [t for t in line.split(',')]
-                filenames.append(tokens[1])
-                earthDetect.append(tokens[2])
-                moonDetect.append(tokens[3])
+                filenames.append(tokens[1]) # Second col is filename
+                earthDetect.append(tokens[2]) # Third column is earth presence
+                moonDetect.append(tokens[3]) # And so on...
                 sunDetect.append(tokens[4])
                 
-        # Binarizes detections and pairs them this their corresponding filename                
+        # Binarizes detections and pairs them this their corresponding filename
+        # dict{key:filename, value:0/1}             
         earthDetect = dict(list(zip(filenames[1:], [0 if i == '' else 1 for i in earthDetect[1:]])))
         moonDetect = dict(list(zip(filenames[1:], [0 if i == '' else 1 for i in moonDetect[1:]])))
         sunDetect = dict(list(zip(filenames[1:], [0 if i == '' else 1 for i in sunDetect[1:]])))
@@ -55,6 +51,8 @@ class Detections(unittest.TestCase):
         earthExp, moonExp, sunExp = self.get_detections(frames)
         frames = [os.path.basename(path) for path in frames]
 
+        # Pairs filename and detection based on find algorithm
+        # dict{key:filename, value:0/1}
         earthDetectExp = [0 if np.isnan(i[0]) else 1 for i in earthExp]
         earthDetectExp = dict(list(zip(frames, earthDetectExp)))
         
@@ -64,25 +62,45 @@ class Detections(unittest.TestCase):
         sunDetectExp = [0 if np.isnan(i[0]) else 1 for i in sunExp]
         sunDetectExp = dict(list(zip(frames, sunDetectExp)))
 
+        # Accumulators for metrics
+        total = len(frames)
+        earthTotal = 0
+        earthWrong = 0
+        moonTotal = 0
+        moonWrong = 0
+        sunTotal = 0
+        sunWrong = 0
 
         for i in earthDetect:
-            if earthDetect[i] == earthDetectExp[i]:
-                pass
-            else:
-                print(i)
+            if earthDetect[i] == earthDetectExp[i] == 1:
+                earthTotal += 1
+            elif earthDetect[i] != earthDetectExp[i]:
+                print("Wrong Earth: ", i)
+                earthWrong += 1
+                earthTotal += 1
 
         for i in moonDetect:
-            if moonDetect[i] == moonDetectExp[i]:
-                pass
-            else:
-                print(i)
-
+            if moonDetect[i] == moonDetectExp[i] == 1:
+                moonTotal += 1
+            elif moonDetect[i] != moonDetectExp[i]:
+                print("Wrong Moon: ", i)
+                moonWrong += 1
+                moonTotal += 1
+            
         for i in sunDetect:
-            if sunDetect[i] == sunDetectExp[i]:
-                pass
-            else:
-                print(i)                
+            if sunDetect[i] == sunDetectExp[i] == 1:
+                sunTotal += 1
+            elif sunDetect[i] != sunDetectExp[i]:
+                print("Sun Wrong: ", i)
+                sunWrong += 1
+                sunTotal += 1
 
+        print("Earth accuracy: ", earthTotal - earthWrong, "/", earthTotal, "=", 1 - earthWrong/earthTotal,)
+        print("Moon accuracy: ", moonTotal - moonWrong, "/", moonTotal, "=", 1 - moonWrong/moonTotal)
+        print("Sun accuracy: ", sunTotal - sunWrong, "/", sunTotal, "=", 1 - sunWrong/sunTotal)
+        print("Total accuracy: ", total - (earthWrong + moonWrong + sunWrong), "/", total, "=", 1 - (earthWrong + moonWrong + sunWrong) / total)
+
+        self.assertEqual(earthWrong + moonWrong + sunWrong, 0, "Find algorithm is not 100% accurate!")
+            
 if __name__ == '__main__':
-    unittest.main()           
-    #test_get_truth_detections()
+    unittest.main()

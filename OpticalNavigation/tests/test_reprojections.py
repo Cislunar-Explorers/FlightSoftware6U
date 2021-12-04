4b8d41cd6ef27f89f879486355176ad581a9d470
Adam Nasir <arn45@cornell.edu>
2021-12-04 13:35:41 -0500

    Initial addition of test

---
+++ OpticalNavigation/tests/test_reprojections.py
@@ -0,0 +1,167 @@
+import cv2
+import numpy as np
+import glob
+import tiled_remap
+import unittest
+
+
+class TestReprojections(unittest.TestCase):
+    """Test is designed to verify the accuracy of image reprojection from gnomonic to
+    stereographic. Takes in the gnomonic image as well as the ideal stereographic from
+    sim, reprojects the former, finds contours, scales them to be the same size, and
+    calculates pixel difference."""
+
+    # Glob gnomonic images as filenames ending in "_gn.png"
+    # and stereographic images as filenames ending in "_st.png"
+    gnomonicList = glob.glob("images/*_gn.png")
+    stereoList = glob.glob("images/*_st.png")
+
+    for gnName in gnomonicList:
+
+        # Load the images
+        src = cv2.imread(gnName)
+        tgt = cv2.imread(gnName.replace("_gn", "_st"))
+
+        gnName = gnName.split("images/")[1]
+
+        cam = tiled_remap.Camera(
+            tiled_remap.radians(62.2), tiled_remap.radians(48.8), 3280, 2464
+        )
+
+        # Determine which camera is used based on whether filename contains "camA", "camB", or "camC"
+        if "camA" in gnName:
+            u_cam = np.array(
+                [5.00000000e-01, 6.12323400e-17, 8.66025404e-01], dtype=np.float32
+            )
+        elif "camB" in gnName:
+            u_cam = np.array(
+                [-5.00000000e-01, -6.12323400e-17, -8.66025404e-01], dtype=np.float32
+            )
+        elif "camC" in gnName:
+            u_cam = (
+                np.array(
+                    [-3.9931775502364646, -3.0090751157602416, 0.0], dtype=np.float32
+                )
+                / 5
+            )
+
+        dt = 18.904e-6
+        omega = 5
+        rot = tiled_remap.CameraRotation(u_cam, -omega * dt)
+
+        bbgn = tiled_remap.BoundingBox(0, 0, cam.w, cam.h)
+        out, bbst = tiled_remap.remap_roi(src, bbgn, cam, rot)
+
+        # Uncomment the following line in order to save the remapped image with filename
+        # "remapped_" + gnName to folder "out"
+        # cv2.imwrite('out/remapped_' + gnName, out)
+
+        # Two largest contours from the remapped image
+        grayOut = cv2.cvtColor(out, cv2.COLOR_BGR2GRAY)
+        _, threshOut = cv2.threshold(grayOut, 0, 255, cv2.THRESH_BINARY)
+        contoursOut, _ = cv2.findContours(
+            threshOut, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+        )
+        contoursOut = sorted(contoursOut, key=cv2.contourArea, reverse=True)[:2]
+
+        # Two largest contours from the target image
+        grayTgt = cv2.cvtColor(tgt, cv2.COLOR_BGR2GRAY)
+        _, threshTgt = cv2.threshold(grayTgt, 0, 255, cv2.THRESH_BINARY)
+        contoursTgt, _ = cv2.findContours(
+            threshTgt, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+        )
+        contoursTgt = sorted(contoursTgt, key=cv2.contourArea, reverse=True)[:2]
+
+        # Compare each contour in contours to each contour in contours2, output
+        for i, c in enumerate(contoursOut):
+
+            for j, c2 in enumerate(contoursTgt):
+
+                x, y, w, h = cv2.boundingRect(c)
+                outc = out[y : y + h, x : x + w]
+
+                x2, y2, w2, h2 = cv2.boundingRect(c2)
+                tgtc = tgt[y2 : y2 + h2, x2 : x2 + w2]
+
+                # Skip if either contour is more than 2 times the size of the other.
+                # They should be close to the same size; if they aren't, they are likely not
+                # the same object.
+                if max(w, h) > 2 * max(w2, h2) or min(w, h) < 0.5 * min(w2, h2):
+                    continue
+
+                # Resize either tgtc or outc to be the size of the largest of the two
+                if w2 > w:
+                    outc = cv2.resize(outc, (w2, h2), interpolation=cv2.INTER_AREA)
+                else:
+                    tgtc = cv2.resize(tgtc, (w, h), interpolation=cv2.INTER_AREA)
+
+                diff = np.average(cv2.subtract(outc, tgtc))
+                cmp = cv2.addWeighted(outc, 0.5, tgtc, 0.5, 0)
+                cmp[np.where((outc != tgtc).all(axis=2))] = [0, 0, 255]
+
+                # Print the name of the image being tested and the difference between the
+                # remapped and target image.
+                print(
+                    "Image ", gnName, " contour ", i, " remapped with difference ", diff
+                )
+
+                minW = 180
+
+                # If the image width is less than 256, resize all three images to have width of 256
+                # and height determined by the aspect ratio of the original image.
+                if w < minW:
+                    outc = cv2.resize(
+                        outc, (minW, int(minW * h / w)), interpolation=cv2.INTER_AREA
+                    )
+                    tgtc = cv2.resize(
+                        tgtc, (minW, int(minW * h / w)), interpolation=cv2.INTER_AREA
+                    )
+                    cmp = cv2.resize(
+                        cmp, (minW, int(minW * h / w)), interpolation=cv2.INTER_AREA
+                    )
+
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                bottomLeftCornerOfText = (0, 12)
+                fontScale = 0.5
+                fontColor = (255, 255, 255)
+                lineType = 1
+
+                cv2.putText(
+                    outc,
+                    "remapped",
+                    bottomLeftCornerOfText,
+                    font,
+                    fontScale,
+                    fontColor,
+                    lineType,
+                )
+                cv2.putText(
+                    tgtc,
+                    "compare",
+                    bottomLeftCornerOfText,
+                    font,
+                    fontScale,
+                    fontColor,
+                    lineType,
+                )
+                cv2.putText(
+                    cmp,
+                    "difference = {:.5f}".format(diff),
+                    bottomLeftCornerOfText,
+                    font,
+                    fontScale,
+                    fontColor,
+                    lineType,
+                )
+
+                composite = cv2.hconcat([outc, tgtc, cmp])
+                outputString = gnName.replace("_gn.png", "_compare")
+
+                # Uncomment the following line in order to save the composite image with filename
+                # "composite_" + outputString to folder "out".
+                # Note that a folder named "comp" needs to be created first.
+                # cv2.imwrite('comp/%s_%s_%s.png' % (outputString,i,j), composite)
+
+
+if __name__ == "__main__":
+    unittest.main()

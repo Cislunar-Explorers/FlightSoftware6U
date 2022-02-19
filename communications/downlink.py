1ee1a7bead6cf55e28a555fa1cf337836f94ce78
Emerald <nullptr@dhcp-vl2041-31006.eduroam.cornell.edu>
2022-02-19 14:35:32 -0500

    Update a comment

--- communications/downlink.py
+++ communications/downlink.py
@@ -1,15 +1,16 @@
 from bitstring import BitArray
 
 
+# TODO add description for bit_inflation's purpose
 def bit_inflation(downlink: bytearray, zero_word: bytes, one_word: bytes):
     # Convert bytes to bits
     downlinkBitString = BitArray(bytes=downlink).bin
 
-    inflatedByteArray = bytearray('', encoding='utf-8')
+    inflatedByteArray = bytearray("", encoding="utf-8")
 
     # Add two bytes for every bit corresponding to the appropriate word
     for bit in downlinkBitString:
-        if bit == '0':
+        if bit == "0":
             inflatedByteArray += zero_word
         else:
             inflatedByteArray += one_word

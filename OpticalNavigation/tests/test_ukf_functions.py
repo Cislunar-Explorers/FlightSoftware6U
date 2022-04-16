235cd23fcfbb72a424781a2ed04f7d0d58605414
Emerald <emeralderisly@gmail.com>
2022-04-15 22:58:05 -0400

    Add .py postfix to test_ukf_functions

--- OpticalNavigation/tests/test_ukf_functions.py
+++ OpticalNavigation/tests/test_ukf_functions.py
@@ -1,8 +1,4 @@
-from core.const import (
-    CovarianceMatrix,
-    EphemerisVector,
-    TrajectoryStateVector,
-)
+from core.const import CovarianceMatrix, EphemerisVector, TrajectoryStateVector
 import numpy as np
 from core.ukf import runTrajUKF
 import unittest
@@ -42,35 +38,41 @@ class TestSequence(unittest.TestCase):
     add [moonEph, sunEph, trajStateVector, dt, P] in @parameterized.expand
     """
 
-    ###################### begin test data ################################
+    """###################### begin test data ################################"""
 
     moonEph1 = EphemerisVector(
-        1.5363e05, -3.7237e05, 2887.6, 0.90889, 0.34863, -0.088026)
+        1.5363e05, -3.7237e05, 2887.6, 0.90889, 0.34863, -0.088026
+    )
     sunEph1 = EphemerisVector(
-        -3.0672e07, -1.441e08, 6670.4, 29.633, -6.0859, -0.00088015)
+        -3.0672e07, -1.441e08, 6670.4, 29.633, -6.0859, -0.00088015
+    )
     trajStateVector1 = TrajectoryStateVector(
-        1.433e05, 5.3541e05, 1.9355e05, -0.93198, -0.077729, 0.049492)
+        1.433e05, 5.3541e05, 1.9355e05, -0.93198, -0.077729, 0.049492
+    )
     dt1 = 1
-    P1 = np.diag(
-        np.array([100, 100, 100, 1e-5, 1e-6, 1e-5], dtype=float))
+    P1 = np.diag(np.array([100, 100, 100, 1e-5, 1e-6, 1e-5], dtype=float))
 
     moonEph2 = EphemerisVector(
-        1.5363e05, -3.7237e05, 2887.6, 0.90889, 0.34863, -0.088026)
+        1.5363e05, -3.7237e05, 2887.6, 0.90889, 0.34863, -0.088026
+    )
     sunEph2 = EphemerisVector(
-        -3.0672e07, -1.441e08, 6670.4, 29.633, -6.0859, -0.00088015)
+        -3.0672e07, -1.441e08, 6670.4, 29.633, -6.0859, -0.00088015
+    )
     trajStateVector2 = TrajectoryStateVector(
-        1.433e05, 5.3541e05, 1.9355e05, -0.93198, -0.077729, 0.049492)
+        1.433e05, 5.3541e05, 1.9355e05, -0.93198, -0.077729, 0.049492
+    )
 
-    ###################### end test data ################################
+    """###################### end test data ################################"""
 
-    @parameterized.expand([
-        [moonEph1, sunEph1, trajStateVector1, dt1, P1],
-        [moonEph2, sunEph2, trajStateVector2, dt1, P1],
-    ])
+    @parameterized.expand(
+        [
+            [moonEph1, sunEph1, trajStateVector1, dt1, P1],
+            [moonEph2, sunEph2, trajStateVector2, dt1, P1],
+        ]
+    )
     def test_sequence(self, moonEph, sunEph, trajStateVector, dt, P):
         print("**********************************\n")
         cameraMeasurements = None
-        cameraParams = None
         main_thrust_info = None
         dynmaicsOnly = True
         trajEstimateOutput = runTrajUKF(
@@ -80,9 +82,8 @@ class TestSequence(unittest.TestCase):
             trajStateVector,
             dt,
             CovarianceMatrix(P),
-            cameraParams,
             main_thrust_info,
-            dynmaicsOnly
+            dynmaicsOnly,
         )
         pos_array = trajEstimateOutput.new_state.get_position_data()
         vel_array = trajEstimateOutput.new_state.get_velocity_data()

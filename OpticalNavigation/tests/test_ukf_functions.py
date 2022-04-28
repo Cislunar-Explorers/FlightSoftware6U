84f3576c5f3dfdd7e5e390baf4e4da3a03ce05ae
Tanya Zhou <tz337@cornell.edu>
2022-04-28 17:59:20 -0400

    added test_iterative_nontruth, and changed some test names

--- OpticalNavigation/tests/test_ukf_functions.py
+++ OpticalNavigation/tests/test_ukf_functions.py
@@ -212,7 +212,7 @@ class TestSequence(unittest.TestCase):
         )
 
     @parameterized.expand(tests_3600_dt)
-    def test_3600_dt_traj2(
+    def test_iterative_truth(
         self,
         moonEph,
         sunEph,
@@ -275,7 +275,7 @@ class TestSequence(unittest.TestCase):
         # assert np.all(np.less_equal(lower, trajEstimateOutput.new_state.data.T))
         # assert np.all(np.less_equal(trajEstimateOutput.new_state.data.T, upper))
 
-    def test_208800_dt_traj2(self):
+    def test_ignore(self):
         trajStateVector = TrajectoryStateVector(
             -2.7699842997503623e4,
             -1.4293835289027357e4,
@@ -301,21 +301,19 @@ class TestSequence(unittest.TestCase):
             cameraMeasurements,
             trajStateVector,
             208800,
-            P,
+            CovarianceMatrix(P),
             main_thrust_info,
             dynamicsOnly,
         )
         trajNew = trajEstimateOutput.new_state.data
         truthState = (
             TrajectoryStateVector(
-                [
                     -1.0680802747723334e5,
                     -2.8316336297967434e5,
                     -1.0300865709131669e5,
                     -0.00874577544564751,
                     -0.6036563933415423,
                     -0.28878799462247133,
-                ]
             )
         ).data
         posError = math.sqrt(
@@ -335,9 +333,69 @@ class TestSequence(unittest.TestCase):
         logging.debug(f"Output x position: {trajNew[0]}\n")
         logging.debug(f"Output y position: {trajNew[1]}\n")
         logging.debug(f"Output z position: {trajNew[2]}\n")
-        print(f"Position error: {posError}\nVelocity error: {velError}")
+        logging.debug(f"Position error: {posError}\nVelocity error: {velError}")
         assert posError <= 1000, "Position error is too large"
         assert velError <= 5, "Velocity error is too large"
+
+    def test_iterative_nontruth(self):
+        trajStateVector = TrajectoryStateVector(
+            -2.7699842997503623E4,
+            -1.4293835289027357E4,
+            1713.3139597599213,
+            -2.8869995362010077,
+            -3.7832739597202594,
+            -.9225267990120935
+        )
+        dynamicsOnly = True
+        main_thrust_info = None
+        cameraMeasurements = None
+        P = CovarianceMatrix(np.diag(np.array([100, 100, 100, 1e-5, 1e-6, 1e-5], dtype=float)))
+        df = pd.read_csv("traj2.csv")
+
+        for index in range(0, df.shape[0]-1):
+            row = df.iloc[[index]]
+            trajEstimateOutput = runTrajUKF(
+                Vector6(
+                        row["mx"],
+                        row["my"],
+                        row["mz"],
+                        0,
+                        0,
+                        0
+                ),
+                Vector6(
+                        row["sx"], row["sy"], row["sz"], 0, 0, 0
+                ),
+                cameraMeasurements,
+                trajStateVector,
+                3600,
+                P,
+                main_thrust_info,
+                dynamicsOnly,
+            )
+            trajStateVector = trajEstimateOutput.new_state
+            P = trajEstimateOutput.new_P
+
+        trajNew = trajStateVector.data
+        truthState = (
+            TrajectoryStateVector(
+                 -1.0680802747723334E5,
+                 -2.8316336297967434E5,
+                 -1.0300865709131669E5,
+                 -.00874577544564751,
+                 -.6036563933415423,
+                 -.28878799462247133)).data
+
+        posError = math.sqrt((trajNew[0] - truthState[0])**2 +
+                             (trajNew[1] - truthState[1])**2 +
+                             (trajNew[2] - truthState[2])**2)
+        velError = math.sqrt((trajNew[3] - truthState[3])**2 +
+                             (trajNew[4] - truthState[4])**2 +
+                             (trajNew[5] - truthState[5])**2)
+        logging.debug(f'Position error: {posError}\nVelocity error: {velError}\n')
+        assert posError <= 1000, 'Position error is too large'
+        assert velError <= 5, 'Velocity error is too large'
+
 
 
 if __name__ == "__main__":
